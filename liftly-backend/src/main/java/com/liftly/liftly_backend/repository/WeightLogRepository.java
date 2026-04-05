c2e63e92d64dd749cc548be19de01ee14badb3ac
AdrianCasasC <casasc.adrian@gmail.com>
2026-04-05 21:08:25 +0200

    (Fix): Run backend

--- liftly-backend/src/main/java/com/liftly/liftly_backend/repository/WeightLogRepository.java
+++ liftly-backend/src/main/java/com/liftly/liftly_backend/repository/WeightLogRepository.java
@@ -1,5 +1,6 @@
 package com.liftly.liftly_backend.repository;
 
+import com.liftly.liftly_backend.model.entity.User;
 import com.liftly.liftly_backend.model.entity.WeightLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
